1f91f79d2c74500027ed208416b7d1d52f712525
Claire Delosa <clair650@gmail.com>
2023-07-15 12:59:37 +1000

    Form created

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django import forms
 from . import views
 
 app_name = 'news'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.StoryView.as_view(), name='story')
+    path('<int:pk>/', views.StoryView.as_view(), name='story'),
+    path('add-story/', views.AddStoryView.as_view(), name='newStory')
 ]
+
+
